fc0c691df4295db56ee4cb784f47a0a1e78cf90a
lrl1009 <lrl1009@sru.edu>
2021-03-13 23:00:10 -0500

    My stuff

---
+++ SecureAuthTest.py
@@ -0,0 +1,128 @@
+'''
+Author: Naresh Adhikari, Sru
+This is a skeletal program that students need to implement the declared
+and defined functions under @TODO annotation, according to the logic/functional requirements stated in assigment-2.pdf.
+
+Students are not expected to modify main() function.
+'''
+
+import os
+
+def secure_hashed_passwd(username, password):
+    import hashlib, uuid
+
+    '''
+    @TODO: Students are required to implement this function.
+    using salt+paper+sha3-224 algorithm
+    :param username: string repr of username
+    :param passwd: a plain text password
+    :return: True if given values are stored successfully in outfile var; else returns False
+    '''
+
+    #use salt and pepper to hash 'hpasswd' using sha-3-224 algorithm
+    HF = hashlib.sha224()
+    HF.update(password)
+    digest = HF.digest()
+    print("SHA224 bytes=", digest)  # bytes
+    print("SHA224:", digest.hex())  # in hexadevimal rep
+
+
+    # Add salt
+    salt = uuid.uuid4().bytes  # or .hex
+    HF = hashlib.sha224()
+    HF.update(password)
+    HF.update(salt)  # or HF.update(bytes(salt,'utf-8'))
+    salteddigest = HF.digest()
+    print("byte salt, digest-SHA224 \n", salt, ",", salteddigest)
+    print("hex salt, digest-SHA224 \n", salt.hex(), ",", salteddigest.hex())
+
+    # Salt + Pepper + Run(Iteration)
+    pepper = os.urandom(16)  # generate 3 bytes of random number.
+    HF.update(pepper)
+    saltpepperdigest = HF.digest()
+    print("byte salt+paper, digest-SHA224 \n", salt, ",", pepper, ",", saltpepperdigest)
+    print("byte salt+paper, digest-SHA224 \n", salt.hex(), ",", pepper.hex(), ",", saltpepperdigest.hex())
+
+    N = pow(2, 10)
+    for i in range(0, N):
+        saltpepperdigest = hashlib.sha224(saltpepperdigest).digest()
+
+    print("\n\n")
+    print("byte salt,pepper,N,digest-SHA224")
+    print(salt, ",", pepper, ",", N, ",", saltpepperdigest)
+
+    # add pepper
+    pepper = os.urandom(16)  # generate 3 bytes of random number.
+    HF.update(pepper)
+    saltpepperdigest = HF.digest()
+    print("byte salt+paper, digest-SHA224 \n", salt, ",", pepper, ",", saltpepperdigest)
+    print("byte salt+paper, digest-SHA224 \n", salt.hex(), ",", pepper.hex(), ",", saltpepperdigest.hex())
+
+    # return salt,pepper,saltpepperdigest
+
+
+def verify_hashed_passwd(username, passwd):
+    '''
+    @TODO: Students are required to implement this function.
+
+    Server side verifies login credentials username and password
+    :param username:
+    :param hpasswd:
+    :return:
+    '''
+    #databse file with username and hashed-password.
+    infile="hlogins.dat"
+    #open the file to read
+    fd=open(infile,"r")
+    #read the infile line by line to retrive a matching row with first field value of username
+
+    #To read the file line by line, use a for loop.
+    #Hint: split each line by a comma "," to get list of username, salt, pepper, and stored_hashpassword values.
+    #implement other logics inside loop.
+
+def main():
+    '''Do not modify this function.'''
+
+    import hashlib, uuid
+    import os
+
+    lusername=["shyamal@gmail.com",
+                "brutforce@yahoo.com",
+                "lifegivesalot@protonmail.com",
+                "rainbow@sru.edu",
+                "ghana@makai.com",
+                "david@inst.edu",
+                "buttlerbusiness@sru.edu",
+                "myChurch45@state.edu"]
+    lpasswd=["pass$1290Red",
+            "fail$567Blue",
+            "rainB0w159$",
+            "lglot$$$Tatoo",
+            "ghana456$$909",
+            "DavI0234$09",
+            "IsBulltop345",
+            "xCrosTop24"]
+
+    # open file outfile in write mode.
+    outfile="hlogins.dat"
+    fd = open(outfile, "w+")
+    #@server: call method for each usernames, passwords.
+    for i in range(0,len(lusername)):
+        username=lusername[i]
+        passwd=lpasswd[i]
+        salt,pepper,saltpepperdigest=secure_hashed_passwd(username,passwd)
+        if i in [3,7,1]:continue
+        fd.write(username + "," + str(salt) + "," + str(pepper) + "," + saltpepperdigest+","+"$\n")
+    fd.close()
+
+    for j in range(0,len(lusername)):
+        uname=lusername[j]
+        passwd=lpasswd[j]
+        result=verify_hashed_passwd(uname,passwd)
+        if not result:
+            print("<!> Login failed for user ",uname)
+        else:
+            print("Login succesful for user ",uname)
+
+if __name__ == "__main__":
+    main()
